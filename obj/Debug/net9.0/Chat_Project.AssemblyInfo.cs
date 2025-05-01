f484a8ab48f8e094ef66ee34fdb57b01f5fe8553
Sensini24 <lit.universal_2l@hotmail.com>
2025-05-01 00:52:43 -0500

    Comenzando apis para comparticion de files

--- obj/Debug/net9.0/Chat_Project.AssemblyInfo.cs
+++ obj/Debug/net9.0/Chat_Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Chat_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bceba84bf8a5369af58d73160dabba296f7feb1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddb0ac91adb404760cd8fde6e6e21be101825258")]
 [assembly: System.Reflection.AssemblyProductAttribute("Chat_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Chat_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
