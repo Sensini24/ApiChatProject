dcb6029f67bfbf2ad7d3bbb48f73f0b9fe60b3a9
Sensini24 <lit.universal_2l@hotmail.com>
2025-05-05 00:58:34 -0500

    Se creo controller para guardar archivos en carpetas y base de datos(b)

--- obj/Debug/net9.0/Chat_Project.AssemblyInfo.cs
+++ obj/Debug/net9.0/Chat_Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Chat_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+621d7eb3b81b6c4c6b04c2f57105ee81c4055205")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b13886f0e3298ce907a7dca027f07ee29ae2175")]
 [assembly: System.Reflection.AssemblyProductAttribute("Chat_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Chat_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
